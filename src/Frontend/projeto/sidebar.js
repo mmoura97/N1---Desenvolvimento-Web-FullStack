e43bd6d76ec68ce24e64cf1b4d0e9a216f30ef00
mmoura97 <151103017+mmoura97@users.noreply.github.com>
2024-04-26 16:13:21 -0300

    Add files via upload

---
+++ src/Frontend/projeto/sidebar.js
@@ -0,0 +1,5 @@
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.querySelector('.sidebar').classList.toggle('open-sidebar');
+});
+
+// toggle funciona como se fosse um if ou seja para abrir a sidebar se clicarmos no botão
